f2a0c83db7d35ffd3f64024805423bc9bc088773
cnJason <chriscyx@vip.qq.com>
2018-01-16 15:51:09 +0800

    inital add Function and Variable Study 👿

---
+++ src/ch1/Function.kt
@@ -0,0 +1,10 @@
+package ch1
+
+
+fun main(args: Array<String>) {
+    sum(3,5)
+}
+
+fun sum(a: Int, b: Int) {
+    println("sum of $a snd $b is ${a + b}")
+}
---
+++ src/ch1/Variable.kt
@@ -0,0 +1,8 @@
+package ch1
+
+fun main(args: Array<String>) {
+
+    val a: Int = 1;
+
+    var x = 5 // 自动推断出int类型.
+}
