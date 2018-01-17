45b91788b02ed07b57c018a98aa3e64326de2452
cnJason <chriscyx@vip.qq.com>
2018-01-17 15:20:39 +0800

    提交。

--- src/utils/ZipUtils.java
+++ src/utils/ZipUtils.java
@@ -45,6 +45,12 @@ public class ZipUtils {
 		try {
 			int byteSum = 0;
 			int byteRead = 0;
+
+			File folderFile = new File(folderPath);
+			if (!folderFile.exists()) {
+				folderFile.mkdir();
+			}
+
 			for (int i = 0; i < originfileList.size(); i++) {
 				File oldfile = new File(originfileList.get(i));
 				if (oldfile.exists()) {
@@ -103,6 +109,86 @@ public class ZipUtils {
 			e.printStackTrace();
 		}
 		return parent;
+	}
+
+
+	/**
+	 * 压缩一个目录
+	 */
+	private static void compressDirectory(File dir, ZipOutputStream zipOut, String baseDir) throws IOException {
+		File[] files = dir.listFiles();
+		for (int i = 0; i < files.length; i++) {
+			compress(files[i], zipOut, baseDir + dir.getName() + "/");
+		}
+	}
+
+	/**
+	 * 压缩一个文件
+	 */
+	private static void compressFile(File file, ZipOutputStream zipOut, String baseDir) throws IOException {
+		if (!file.exists()) {
+			return;
+		}
+
+		BufferedInputStream bis = null;
+		try {
+			bis = new BufferedInputStream(new FileInputStream(file));
+			ZipEntry entry = new ZipEntry(baseDir + file.getName());
+			zipOut.putNextEntry(entry);
+			int count;
+			byte data[] = new byte[BUFFER];
+			while ((count = bis.read(data, 0, BUFFER)) != -1) {
+				zipOut.write(data, 0, count);
+			}
+
+		} finally {
+			if (null != bis) {
+				bis.close();
+			}
+		}
+	}
+
+
+	static final int BUFFER = 8192;
+
+	/**
+	 * 压缩文件夹.
+	 * @param srcPath
+	 * @throws IOException
+	 */
+	public static void compress(String srcPath) throws IOException {
+		File srcFile = new File(srcPath);
+		File dstFile = new File(dstPath + ".zip");
+		if (!srcFile.exists()) {
+			throw new FileNotFoundException(srcPath + "不存在！");
+		}
+
+		FileOutputStream out = null;
+		ZipOutputStream zipOut = null;
+		try {
+			out = new FileOutputStream(dstFile);
+			CheckedOutputStream cos = new CheckedOutputStream(out, new CRC32());
+			zipOut = new ZipOutputStream(cos);
+			String baseDir = "";
+			compress(srcFile, zipOut, baseDir);
+		} finally {
+			if (null != zipOut) {
+				zipOut.close();
+				out = null;
+			}
+
+			if (null != out) {
+				out.close();
+			}
+		}
+	}
+
+	private static void compress(File file, ZipOutputStream zipOut, String baseDir) throws IOException {
+		if (file.isDirectory()) {
+			compressDirectory(file, zipOut, baseDir);
+		} else {
+			compressFile(file, zipOut, baseDir);
+		}
 	}
 
 
